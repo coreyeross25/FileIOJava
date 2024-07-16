0f96307fcf4cb933f33b1919a152b7b68a8dfe0b
coreyeross25 <coreyeross12@gmail.com>
2024-07-15 20:58:59 -0400

    Step two completed

--- Step2/ReadTwo.java
+++ Step2/ReadTwo.java
@@ -9,7 +9,7 @@ public class ReadTwo
     public static void main(String[] args)
     {
         try {
-            Scanner fileIn = new Scanner(new File("input.txt"));
+            Scanner fileIn = new Scanner(new File("/Users/coreye/Projects/FileIOJava/Step2/input.txt"));
 
             while (fileIn.hasNext()) 
             {
